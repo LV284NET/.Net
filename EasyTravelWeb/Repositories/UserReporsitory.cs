0bdcd79b17aed26e8f76c82d4aea8e4e5183ab51
pavlokoz <shostakpavlo@gmail.com>
2017-12-20 16:07:24 +0200

    CreateMethodForAddUser

--- EasyTravelWeb/Repositories/UserReporsitory.cs
+++ EasyTravelWeb/Repositories/UserReporsitory.cs
@@ -2,6 +2,8 @@
 using EasyTravelWeb.Models;
 using System.Data.SqlClient;
 using System.Data;
+using System;
+
 
 namespace EasyTravelWeb.Repositories
 {
@@ -9,11 +11,13 @@ namespace EasyTravelWeb.Repositories
     {
         public User GetUser(string eMail, string password)
         {
-            using (SqlConnection connection=new SqlConnection(ConfigurationManager.ConnectionStrings["EasyTravelConnectionString"].ConnectionString))
+            using (SqlConnection connection =
+                new SqlConnection(ConfigurationManager.ConnectionStrings["EasyTravelConnectionString"]
+                    .ConnectionString))
             {
                 connection.Open();
 
-                SqlCommand command=new SqlCommand("GetUser", connection);
+                SqlCommand command = new SqlCommand("GetUser", connection);
 
                 command.CommandType = CommandType.StoredProcedure;
 
@@ -23,9 +27,69 @@ namespace EasyTravelWeb.Repositories
                 using (SqlDataReader reader = command.ExecuteReader())
                 {
                     reader.Read();
-                    return new User {FirstName = reader["firstName"].ToString(), LastName = reader["lastName"].ToString(), Email = reader["email"].ToString()};
+                    return new User
+                    {
+                        FirstName = reader["firstName"].ToString(),
+                        LastName = reader["lastName"].ToString(),
+                        Email = reader["email"].ToString()
+                    };
                 }
             }
+        }
+
+        private bool isNewUser(string eMail)
+        {
+            using (SqlConnection connection =
+                new SqlConnection(ConfigurationManager.ConnectionStrings["EasyTravelConnectionString"]
+                    .ConnectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("isNewUser", connection);
+
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@Email", eMail));
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (!reader.HasRows)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        public bool AddUser(string eMail, string password, string firstName, string lastName)
+        {
+            if (this.isNewUser((eMail)))
+            {
+                using (SqlConnection connection =
+                    new SqlConnection(ConfigurationManager.ConnectionStrings["EasyTravelConnectionString"]
+                        .ConnectionString))
+                {
+                    connection.Open();
+
+                    SqlCommand command = new SqlCommand("InsertNewUser", connection);
+
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    command.Parameters.Add(new SqlParameter("@Email", eMail));
+                    command.Parameters.Add(new SqlParameter("@Password", password));
+                    command.Parameters.Add(new SqlParameter("@FirstName", firstName));
+                    command.Parameters.Add(new SqlParameter("@LastName", lastName));
+                    command.Parameters.Add(new SqlParameter("UserID", Guid.NewGuid()));
+
+                    command.ExecuteNonQuery();
+
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
