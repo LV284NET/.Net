dcfac0fc5f4bfdd74914a29e42ac716fae31fc85
pavlokoz <shostakpavlo@gmail.com>
2017-12-20 14:40:29 +0200

    AddUserRepository

--- EasyTravelWeb/Controllers/UserController.cs
+++ EasyTravelWeb/Controllers/UserController.cs
@@ -5,66 +5,18 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using EasyTravelWeb.Repositories;
 
 namespace EasyTravelWeb.Controllers
 {
     public class UserController : ApiController
     {
-        IList<User> users = new List<User>()
-        {
-            new User()
-            {
-                UserId=1, Email="user1@gmail.com", FirstName="Jhon", LastName="Smith", Password="1111"
-            },
-
-            new User()
-            {
-                UserId=2, Email="user2@gmail.com", FirstName="Poll", LastName="Mithel", Password="1111"
-            },
-
-            new User()
-            {
-                UserId=3, Email="user3@gmail.com", FirstName="Rachel", LastName="Green", Password="1111"
-            },
-
-            new User()
-            {
-                UserId=4, Email="user4@gmail.com", FirstName="Sarah", LastName="Bernar", Password="1111"
-            },
-        };
-
-        [HttpGet]
-        public IList<User> GetAllUsers()
-        {
-            //Return list of all users  
-            return this.users;
-        }
-
-        public User GetUserDetails(int id)
-        {
-            //Return a single employee detail  
-            var users = this.users.FirstOrDefault(e => e.UserId == id);
-            if (users == null)
-            {
-                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
-            }
-
-            return users;
-        }
-
-        public class MyUser
-        {
-            public string Email { get; set; }
-            public string Password { get; set; }
-
-
-        }
+        private UserRepository userRepository =new UserRepository();
 
         [HttpPost]
-        public User GetUser([FromBody] MyUser myUser)
+        public User GetUser([FromBody] User myUser)
         {
-            return users?.First(row =>
-                    row.Email == myUser.Email && row.Password == myUser.Password);
+            return userRepository.GetUser(myUser.Email, myUser.Password);
         }
     }
 }
---
+++ EasyTravelWeb/Repositories/UserReporsitory.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+using EasyTravelWeb.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace EasyTravelWeb.Repositories
+{
+    public class UserRepository
+    {
+        public User GetUser(string eMail, string password)
+        {
+            using (SqlConnection connection=new SqlConnection(ConfigurationManager.ConnectionStrings["EasyTravelConnectionString"].ConnectionString))
+            {
+                connection.Open();
+
+                SqlCommand command=new SqlCommand("GetUser", connection);
+
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.Add(new SqlParameter("@Email", eMail));
+                command.Parameters.Add(new SqlParameter("@Password", password));
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    reader.Read();
+                    return new User {FirstName = reader["firstName"].ToString(), LastName = reader["lastName"].ToString(), Email = reader["email"].ToString()};
+                }
+            }
+        }
+    }
+}
