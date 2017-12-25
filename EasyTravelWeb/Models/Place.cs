7ed3674349a193af34f9ff2add010003c947361a
pavlokoz <shostakpavlo@gmail.com>
2017-12-25 14:30:23 +0200

    AddMethodForGetImage

--- EasyTravelWeb/Models/Place.cs
+++ EasyTravelWeb/Models/Place.cs
@@ -4,13 +4,15 @@ namespace EasyTravelWeb.Models
 {
     public class Place
     {
+        public int PlaceId { get; set; }
+
         public string Name { get; set; }
 
         public string CityName { get; set; }
 
         public string Description { get; set; }
 
-        public Image PicturePlace { get; set; }
+//        public Image PicturePlace { get; set; }
 
         public Place()
         {
@@ -22,7 +24,7 @@ namespace EasyTravelWeb.Models
             this.Name = name;
             this.CityName = cityName;
             this.Description = description;
-            this.PicturePlace = picturePlace;
+//            this.PicturePlace = picturePlace;
         }
     }
 }
