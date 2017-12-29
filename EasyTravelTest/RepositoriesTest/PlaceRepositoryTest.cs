93f27df0fa4fd5460a8328a08c2740cad1b35b8c
pavlokoz <shostakpavlo@gmail.com>
2017-12-29 14:27:15 +0200

    CreateTestPlaceRepository

---
+++ EasyTravelTest/RepositoriesTest/PlaceRepositoryTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using EasyTravelWeb.Controllers;
+using EasyTravelWeb.Models;
+using EasyTravelWeb.Repositories;
+using NUnit.Framework;
+
+namespace EasyTravelTest.RepositoriesTest
+{
+    /// <summary>
+    /// Summary description for PlaceRepositoryTest
+    /// </summary>
+    [TestFixture]
+    public class PlaceRepositoryTest
+    {
+        [TestCase]
+        public void GetPlace()
+        {
+            PlaceRepository placeRepository = new PlaceRepository(); 
+            Assert.AreNotEqual(null, placeRepository.GetPlace(new Random().Next(1, 28)));
+        }
+    }
+}
