632aaae428bf6b9276197377570b6000998545a6
Trush Oleh <trusholegi@gmail.com>
2018-01-04 15:04:00 +0200

    Add PlaceController and supplemented PlaceRepository

--- EasyTravelWeb/Repositories/PlaceRepository.cs
+++ EasyTravelWeb/Repositories/PlaceRepository.cs
@@ -45,5 +45,40 @@ namespace EasyTravelWeb.Repositories
                 }
             }
         }
+        public Place GetPlaceByCytiName(string placeName)
+        {
+            using (SqlConnection connection =
+                new SqlConnection(ConfigurationManager.ConnectionStrings["EasyTravelConnectionString"]
+                    .ConnectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand("GetTopPlaceByName", connection);
+
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.Add(new SqlParameter("@Place", placeName));
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return new Place
+                        {
+                            PlaceId = Convert.ToInt32(reader["PlaceID"]),
+                            Name = reader["PlaceName"].ToString(),
+                            CityName = reader["CityName"].ToString(),
+                            Description = reader["PlaceDescription"].ToString(),
+                            PicturePlace = reader["MainPlaceImage"].ToString()
+                        };
+                    }
+                    else
+                    {
+                        return this.GetPlaceByCytiName(placeName);
+                    }
+                }
+            }
+        }
     }
 }
+  
