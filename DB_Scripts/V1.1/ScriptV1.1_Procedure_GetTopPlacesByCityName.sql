1e5d1a3cd4c36412080f7a53fed6e3507d4ad010
pavlokoz <shostakpavlo@gmail.com>
2018-01-10 15:03:05 +0200

    FinalTry_OneScript_OneProcedure

---
+++ DB_Scripts/V1.1/ScriptV1.1_Procedure_GetTopPlacesByCityName.sql
@@ -0,0 +1,13 @@
+﻿CREATE procedure [dbo].[GetTopPlacesByCityName] (@CityName nvarchar(40))
+as
+
+set transaction isolation level read committed 
+
+begin transaction
+
+	select top 4 * from [Place]
+	where CityID = (select [CityID] 
+					from [dbo].[City]
+					where [CityName] = @CityName)
+
+commit transaction
