4b7f07d719d1fde3d047d76fb6196025cc60446b
pavlokoz <shostakpavlo@gmail.com>
2018-01-10 14:18:42 +0200

    FIXERRORS

--- DB_Scripts/V1.2/ScriptV1.2_AddProcedures.sql
+++ DB_Scripts/V1.2/ScriptV1.2_AddProcedures.sql
@@ -1,25 +1,32 @@
-﻿CREATE procedure GetPlacesByCityId (@CityID bigint)
-as
-
-set transaction isolation level read committed 
-
-begin transaction	
-	select [City].[CityName], [Place].PlaceID, [Place].PlaceName, [Place].PlaceDescription, [Place].MainPlaceImage
-	from [Place] inner join [City]	
-	on [Place].CityID = [City].CityID
-	where @CityID = [Place].CityID;
-commit transaction
-
+﻿SET ANSI_NULLS ON
+GO
 SET QUOTED_IDENTIFIER ON
-
-create procedure GetTopPlacesByCityId (@CityID bigint)
+GO
+CREATE procedure [dbo].[GetCityById](@CityID bigint)
 as
 
 set transaction isolation level read committed 
 
 begin transaction
-
-	select top 4 * from [Place]
-	where CityID = @CityID;
-
+	select * from [City]
+	where @CityID = [CityID]	
 commit transaction
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE procedure [dbo].[GetPlaceByName] (@PlaceName nvarchar(40))
+as
+
+set transaction isolation level read committed
+
+begin transaction
+	select [City].[CityName], [Place].PlaceID, [Place].PlaceName, [Place].PlaceDescription, [Place].MainPlaceImage
+	from [Place] inner join [City]	
+	on [Place].CityID = [City].CityID
+	where @PlaceName = [Place].PlaceName;
+commit transaction
+
+GO
