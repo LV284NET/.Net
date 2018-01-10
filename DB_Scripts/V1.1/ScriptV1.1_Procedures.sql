a866f7e3fbde89a222141807798ac7dd39c75874
pavlokoz <shostakpavlo@gmail.com>
2018-01-10 13:55:13 +0200

    FGF

--- DB_Scripts/V1.1/ScriptV1.1_Procedures.sql
+++ DB_Scripts/V1.1/ScriptV1.1_Procedures.sql
@@ -6,3 +6,4 @@ set transaction isolation level read committed
 begin transaction
 	select * from [City]	
 commit transaction
+
---
+++ DB_Scripts/V1.2/ScriptV1.2_AddProcedures.sql
@@ -0,0 +1,30 @@
+﻿CREATE procedure [dbo].[GetPlacesByCityId] (@CityID bigint)
+as
+
+set transaction isolation level read committed 
+
+begin transaction
+	--select * from [Place]
+	-- where CityID = @CityID;
+
+	select [City].[CityName], [Place].PlaceID, [Place].PlaceName, [Place].PlaceDescription, [Place].MainPlaceImage
+	from [Place] inner join [City]	
+	on [Place].CityID = [City].CityID
+	where @CityID = [Place].CityID;
+commit transaction
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+create procedure [dbo].[GetTopPlacesByCityId] (@CityID bigint)
+as
+
+set transaction isolation level read committed 
+
+begin transaction
+
+	select top 4 * from [Place]
+	where CityID = @CityID;
+
+commit transaction
+GO
