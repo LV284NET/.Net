011ebcd2c6b4285deca15d64daf25b4b73d0163b
Dmytro Ratynchuk <ratynchuk@gmail.com>
2018-01-15 15:55:55 +0200

    ScriptV1.3

---
+++ DB_Scripts/V1.3/ScriptV1.3_Alter_Table_AspNetUsers_Add_FirstName_And_LastName_Columns.sql
@@ -0,0 +1,12 @@
+﻿USE [Lv-284.Net_EasyTravel]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER TABLE [dbo].[AspNetUsers]
+	ADD	[FirstName] [nvarchar](max) NULL,
+		[LastName] [nvarchar](max) NULL
