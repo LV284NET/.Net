709672133b36da6c0225ce326911b1eff28efcfe
Dmytro Ratynchuk <ratynchuk@gmail.com>
2018-01-15 16:03:36 +0200

    Updated ScriptV1.3

--- DB_Scripts/V1.3/ScriptV1.3_Alter_Table_AspNetUsers_Add_FirstName_And_LastName_Columns.sql
+++ DB_Scripts/V1.3/ScriptV1.3_Alter_Table_AspNetUsers_Add_FirstName_And_LastName_Columns.sql
@@ -1,12 +1,3 @@
-﻿USE [Lv-284.Net_EasyTravel]
-GO
-
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
-ALTER TABLE [dbo].[AspNetUsers]
+﻿ALTER TABLE [dbo].[AspNetUsers]
 	ADD	[FirstName] [nvarchar](max) NULL,
 		[LastName] [nvarchar](max) NULL
