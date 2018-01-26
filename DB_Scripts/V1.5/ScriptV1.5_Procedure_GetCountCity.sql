6162ee055fab2cd46d48b8ce33d4da20ba417d60
pavlokoz <shostakpavlo@gmail.com>
2018-01-26 14:02:41 +0200

    FixProcedure

--- DB_Scripts/V1.5/ScriptV1.5_Procedure_GetCountCity.sql
+++ DB_Scripts/V1.5/ScriptV1.5_Procedure_GetCountCity.sql
@@ -1,10 +1,3 @@
-﻿USE [Lv-284.Net_EasyTravel]
-GO
-/****** Object:  StoredProcedure [dbo].[GetCountCity]    Script Date: 2018-01-23 1:05:28 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE procedure [dbo].[GetCountCity]
+﻿CREATE procedure [dbo].[GetCountCity]
 as
 select COUNT (*) from [City]
--- DB_Scripts/V1.5/ScriptV1.5_Procedure_GetCountPlace.sql
+++ DB_Scripts/V1.5/ScriptV1.5_Procedure_GetCountPlace.sql
@@ -1,10 +1,3 @@
-﻿USE [Lv-284.Net_EasyTravel]
-GO
-/****** Object:  StoredProcedure [dbo].[GetCountPlace]    Script Date: 2018-01-23 1:41:53 PM ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-ALTER procedure [dbo].[GetCountPlace]
+﻿Create procedure [dbo].[GetCountPlace]
 as
 select COUNT (*) from [Place]	
