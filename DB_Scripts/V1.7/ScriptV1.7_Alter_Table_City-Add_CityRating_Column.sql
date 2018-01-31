07cfaaca617862cacb569d2dfe7120efd17cd794
pavlokoz <shostakpavlo@gmail.com>
2018-01-31 20:45:11 +0200

    WorkedScriptsWithCheckExist

--- DB_Scripts/V1.7/ScriptV1.7_Alter_Table_City-Add_CityRating_Column.sql
+++ DB_Scripts/V1.7/ScriptV1.7_Alter_Table_City-Add_CityRating_Column.sql
@@ -1,4 +1,4 @@
-﻿IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo]City]') AND type in (N'U'))
+﻿IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[City]') AND type in (N'U'))
 BEGIN
 ALTER TABLE [dbo].[City]
 	ADD	[CityRating] [decimal](18,2) DEFAULT(0)
