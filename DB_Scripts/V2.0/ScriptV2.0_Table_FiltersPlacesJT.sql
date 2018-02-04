85d0b7f17a20a92852026e32bb3993f3e5192d3a
Dmytro Ratynchuk <ratynchuk@gmail.com>
2018-02-04 10:49:17 +0200

    updated scripts

--- DB_Scripts/V2.0/ScriptV2.0_Table_FiltersPlacesJT.sql
+++ DB_Scripts/V2.0/ScriptV2.0_Table_FiltersPlacesJT.sql
@@ -1,18 +1,15 @@
-﻿IF EXISTS (SELECT * FROM sys.objects Where name = 'FiltersPlacesJT')
+﻿IF NOT EXISTS (SELECT * FROM sys.objects Where name = 'FiltersPlacesJT')
 BEGIN
-	DROP TABLE [dbo].[FiltersPlacesJT]
-END
-GO
-
-CREATE TABLE [dbo].[FiltersPlacesJT](
+	CREATE TABLE [dbo].[FiltersPlacesJT](
 	[FilterId] [bigint] NOT NULL,
 	[PlaceId] [bigint] NOT NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[FilterId] ASC,
-	[PlaceId] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
+	PRIMARY KEY CLUSTERED 
+	(
+		[FilterId] ASC,
+		[PlaceId] ASC
+	)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+	) ON [PRIMARY]
+END
 GO
 
 ALTER TABLE [dbo].[FiltersPlacesJT]  WITH CHECK ADD FOREIGN KEY([FilterId])
