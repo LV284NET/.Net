96593bcbd024690db35ffb0fef4567f4f4c623ea
pavlokoz <shostakpavlo@gmail.com>
2018-02-04 11:22:14 +0200

    FixScriptsForInsert

--- DB_Scripts/V2.0/ScriptV2.0_Insert_Filters.sql
+++ DB_Scripts/V2.0/ScriptV2.0_Insert_Filters.sql
@@ -1,22 +1,16 @@
-SET IDENTITY_INSERT [dbo].[Filters] ON 
-GO
-INSERT [dbo].[Filters] ([FilterId], [FilterName]) VALUES (1, N'Monument')
-GO
-INSERT [dbo].[Filters] ([FilterId], [FilterName]) VALUES (2, N'Church')
-GO
-INSERT [dbo].[Filters] ([FilterId], [FilterName]) VALUES (3, N'Food&Drink')
-GO
-INSERT [dbo].[Filters] ([FilterId], [FilterName]) VALUES (4, N'Theater')
-GO
-INSERT [dbo].[Filters] ([FilterId], [FilterName]) VALUES (5, N'Museum')
-GO
-INSERT [dbo].[Filters] ([FilterId], [FilterName]) VALUES (6, N'Park')
-GO
-INSERT [dbo].[Filters] ([FilterId], [FilterName]) VALUES (7, N'Shop')
-GO
-INSERT [dbo].[Filters] ([FilterId], [FilterName]) VALUES (8, N'Entertaiment')
-GO
-INSERT [dbo].[Filters] ([FilterId], [FilterName]) VALUES (9, N'Sightseeing')
-GO
-INSERT [dbo].[Filters] ([FilterId], [FilterName]) VALUES (10, N'Bar')
-GO
+﻿IF EXISTS (SELECT * FROM sys.objects WHERE name = 'Filters')
+BEGIN
+	SET IDENTITY_INSERT [dbo].[Filters] ON 
+	INSERT [dbo].[Filters] ([FilterId], [FilterName]) VALUES (1, N'Monument')
+	INSERT [dbo].[Filters] ([FilterId], [FilterName]) VALUES (2, N'Church')
+	INSERT [dbo].[Filters] ([FilterId], [FilterName]) VALUES (3, N'Food&Drink')
+	INSERT [dbo].[Filters] ([FilterId], [FilterName]) VALUES (4, N'Theater')
+	INSERT [dbo].[Filters] ([FilterId], [FilterName]) VALUES (5, N'Museum')
+	INSERT [dbo].[Filters] ([FilterId], [FilterName]) VALUES (6, N'Park')
+	INSERT [dbo].[Filters] ([FilterId], [FilterName]) VALUES (7, N'Shop')
+	INSERT [dbo].[Filters] ([FilterId], [FilterName]) VALUES (8, N'Entertaiment')
+	INSERT [dbo].[Filters] ([FilterId], [FilterName]) VALUES (9, N'Sightseeing')
+	INSERT [dbo].[Filters] ([FilterId], [FilterName]) VALUES (10, N'Bar')
+	SET IDENTITY_INSERT [dbo].[Filters] OFF
+END
+	
--- DB_Scripts/V2.0/ScriptV2.0_Insert_FiltersPlacesJT.sql
+++ DB_Scripts/V2.0/ScriptV2.0_Insert_FiltersPlacesJT.sql
@@ -1,194 +1,101 @@
-﻿SET IDENTITY_INSERT [dbo].[Filters] OFF
-GO
+﻿IF EXISTS (SELECT * FROM sys.objects WHERE name = 'FiltersPlacesJT')
+BEGIN
+SET IDENTITY_INSERT [dbo].[FiltersPlacesJT] ON 
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (1, 10)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (1, 12)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (1, 23)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (1, 24)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (1, 25)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (1, 58)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (1, 62)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (2, 5)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (2, 6)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (2, 20)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (2, 61)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (3, 1)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (3, 8)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (3, 9)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (3, 11)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (3, 18)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (3, 21)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (3, 26)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (4, 3)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (4, 44)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (4, 50)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (5, 4)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (5, 7)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (5, 8)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (5, 22)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (5, 28)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (5, 30)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (5, 31)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (5, 32)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (5, 33)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (5, 39)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (5, 46)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (5, 56)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (6, 13)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (6, 19)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (6, 26)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (6, 29)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (6, 34)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (6, 40)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (6, 45)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (6, 51)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (6, 55)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (6, 57)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (6, 60)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (7, 1)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (7, 11)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (7, 16)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (8, 1)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (8, 9)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (8, 12)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (8, 15)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (8, 16)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (8, 36)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (8, 37)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (8, 38)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (8, 47)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (8, 55)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (8, 60)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 1)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 2)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 3)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 4)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 5)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 6)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 10)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 12)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 13)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 14)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 17)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 18)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 19)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 20)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 23)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 26)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 27)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 28)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 29)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 31)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 34)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 35)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 39)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 40)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 45)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 47)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 48)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 49)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 50)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 52)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 53)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 54)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 55)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 57)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 61)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (9, 62)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (10, 9)
-GO
 INSERT [dbo].[FiltersPlacesJT] ([FilterId], [PlaceId]) VALUES (10, 21)
-GO
+SET IDENTITY_INSERT [dbo].[FiltersPlacesJT] OFF
+
