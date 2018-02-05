4c6e528da7b70d21605403112015cf01bd3ba336
pavlokoz <shostakpavlo@gmail.com>
2018-02-05 13:39:50 +0200

    FixProcedure

---
+++ DB_Scripts/V2.2/ScriptV2.2_Procedure_GetCitiesIdAndNames.sql
@@ -0,0 +1,13 @@
+﻿IF EXISTS (SELECT * FROM sys.objects WHERE name = 'GetCitiesIdsAndNames')
+BEGIN
+	DROP PROCEDURE [dbo].[GetCitiesIdsAndNames]
+END
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE name = 'GetCitiesIdAndNames')
+BEGIN
+	DROP PROCEDURE [dbo].[GetCitiesIdAndNames]
+END
+GO
+CREATE procedure [dbo].[GetCitiesIdAndNames]
+as
+select CityID, CityName from [City]	
