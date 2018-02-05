2838e117e3280e2aa911b7c50a76a587af5f63f4
pavlokoz <shostakpavlo@gmail.com>
2018-02-05 16:08:47 +0200

    AddNewScript

---
+++ DB_Scripts/V2.3/ScriptV2.3_Procedure_GetUserFavoritePlaces.sql
@@ -0,0 +1,13 @@
+﻿IF EXISTS (SELECT * FROM sys.objects WHERE name = 'GetUserFavoritePlaces')
+BEGIN
+	DROP PROCEDURE [dbo].[GetUserFavoritePlaces]
+END
+GO
+CREATE procedure [dbo].[GetUserFavoritePlaces] (@UserID int)
+as
+	select [City].[CityName], [Place].PlaceID, [Place].PlaceName, [Place].MainPlaceImage, [Place].CityID
+	from [Place] inner join [City]	
+	on [Place].CityID = [City].CityID
+	inner join [Favorite]
+	on [Place].PlaceID = [Favorite].PlaceId
+	where [Favorite].UserId = @UserID;
